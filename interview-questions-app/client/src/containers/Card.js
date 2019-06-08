a633fa6617dae2f5a2386d41f0d8bd3970a80066
Monica <mntamaru@gmail.com>
2019-06-08 16:48:00 -0700

    add Card.js and Card.css

---
+++ interview-questions-app/client/src/containers/Card.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Card.css';
+// card and card.css from https://stackblitz.com/edit/react-flashcards
+
+//this is a functional and presentational component
+const Card = (props) => (
+  <div className='card-container'>
+    <div className='card'>
+      <div className='front'>
+        <div className='question'>{props.question}</div>
+      </div>
+      <div className='back'>
+        <div className='answer'>{props.answer}</div>
+        <div className='description'>{props.description}</div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Card;
